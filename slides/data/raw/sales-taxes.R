29635d45dcad920b9280bce1484cf83fbf7b7c3e
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2025-09-17 18:58:31 -0400

    Some updates

---
+++ slides/data/raw/sales-taxes.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(janitor)
+
+sales_taxes_raw <- read_csv(
+  here::here(
+    "slides/data/raw/2025 Sales Tax Rates  Sales Taxes by State.csv"
+  )
+)
+
+sales_taxes <- sales_taxes_raw |>
+  clean_names() |>
+  mutate(across(where(is.character), str_remove, "%")) |>
+  mutate(across(contains("tax"), as.numeric))
+
+sales_taxes |>
+  write_csv(here::here("slides/data/sales-taxes-25.csv"))
