6b02f4946426c6585fa138c88f01ad22ffce27dd
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2025-11-23 21:53:04 -0500

    Post Lab 7

---
+++ data/rdu-flights/_rdu-flights-data-prep.R
@@ -0,0 +1,48 @@
+# load packages ----------------------------------------------------------------
+
+library(arrow)
+library(tidyverse)
+
+all_data <- read_parquet(here::here("lab/data/rdu-flights/data_0.parquet"))
+
+# prep data --------------------------------------------------------------------
+
+rdu_flights <- all_data |>
+  filter(Origin == "RDU") |>
+  janitor::clean_names() |>
+  select(
+    year,
+    quarter,
+    flight_date,
+    month,
+    day_of_month = dayof_month,
+    day_of_week,
+    carrier_code = reporting_airline,
+    origin,
+    origin_city = origin_city_name,
+    dest,
+    dest_city = dest_city_name,
+    dep_time,
+    dep_delay,
+    dep_delay_15 = dep_del15,
+    arr_time,
+    arr_delay,
+    arr_delay_15 = arr_del15,
+    crs_elapsed_time,
+    actual_elapsed_time,
+    air_time,
+    distance
+  ) |>
+  mutate(
+    air_time = as.numeric(air_time)
+  ) |>
+  drop_na() |>
+  right_join(
+    nycflights13::airlines |> rename(carrier_name = name),
+    join_by(carrier_code == carrier)
+  ) |>
+  relocate(carrier_name, .after = carrier_code)
+
+# write data -----------------------------------------------------------------------
+
+write_csv(rdu_flights, file = "lab/data/rdu-flights.csv")
